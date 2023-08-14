9ac3bc65a2510010407a5aa3813d80ac0fef4eec
Mateusz Borsuk <mtszborsuk212@gmail.com>
2023-08-15 00:13:07 +0200

    [WIP] First commit of the AWS CDK Glue Stack

--- budgetguard/budgetguard/cdk_infrastructure/app.py
+++ budgetguard/budgetguard/cdk_infrastructure/app.py
@@ -3,6 +3,10 @@ from aws_cdk import App
 from aws_cdk import Environment
 from cdk_infrastructure.lambda_function.stack import IngestionLambdaStack
 from cdk_infrastructure.s3_buckets.stack import S3DeployStack
+from cdk_infrastructure.glue.stack import (
+    GlueDataCatalogStack,
+    GlueCrawlersStack,
+)
 from cdk_infrastructure.s3_buckets.constants import BUCKET_NAMES
 from dotenv import load_dotenv
 
@@ -22,5 +26,12 @@ IngestionLambdaStack(
 for bucket_name in BUCKET_NAMES:
     S3DeployStack(app, f"{bucket_name}Stack", bucket_id=bucket_name, env=env)
 
+catalog = GlueDataCatalogStack(app, "GlueDataCatalogStack", env=env)
+
+GlueCrawlersStack(
+    app,
+    "GlueCrawlersStack",
+    env=env,
+)
 
 app.synth()
