4e38e02694ab2bef7d4d6b3d867f661d7a3dba59
Mateusz Borsuk <mtszborsuk212@gmail.com>
2023-08-19 18:02:43 +0200

    Added bronze glue catalog and crawler

--- budgetguard/budgetguard/cdk_infrastructure/cdk_infrastructure/glue/stack.py
+++ budgetguard/budgetguard/cdk_infrastructure/cdk_infrastructure/glue/stack.py
@@ -7,8 +7,8 @@ class GlueDataCatalogStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create a Glue Data Catalog database
-        database = glue.CfnDatabase(
+        # Create a Ingest Glue Data Catalog database
+        glue.CfnDatabase(
             self,
             "budget-guard-ingest-database",
             catalog_id=Aws.ACCOUNT_ID,
@@ -18,14 +18,23 @@ class GlueDataCatalogStack(Stack):
             },
         )
 
-        self.database = database
+        # Create a Bronze Glue Data Catalog database
+        glue.CfnDatabase(
+            self,
+            "budget-guard-bronze-database",
+            catalog_id=Aws.ACCOUNT_ID,
+            database_input={
+                "name": "budget-guard-bronze-database",
+                "description": "AWS Glue Database for bronze",
+            },
+        )
 
 
 class GlueCrawlersStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create a Glue Crawler
+        # Create a Ingestion Glue Crawler
         glue.CfnCrawler(
             self,
             "budget-guard-ingest-crawler",
@@ -33,5 +42,16 @@ class GlueCrawlersStack(Stack):
             role=f"arn:aws:iam::{Aws.ACCOUNT_ID}:role/service-role/AWSGlueServiceRole",  # noqa
             database_name="budget-guard-ingest-database",
             targets={"s3Targets": [{"path": "s3://budget-guard-ingest"}]},
-            # schedule={"scheduleExpression": "cron(0 0 * * ? *)"},
+            # schedule=glue.CfnCrawler.ScheduleProperty(schedule_expression="cron(0 1 * * ? *)"),  # noqa
+        )
+
+        # Create a Bronze Glue Crawler
+        glue.CfnCrawler(
+            self,
+            "budget-guard-bronze-crawler",
+            name="budget-guard-bronze-crawler",
+            role=f"arn:aws:iam::{Aws.ACCOUNT_ID}:role/service-role/AWSGlueServiceRole",  # noqa
+            database_name="budget-guard-bronze-database",
+            targets={"s3Targets": [{"path": "s3://budget-guard-bronze"}]},
+            # schedule=glue.CfnCrawler.ScheduleProperty(schedule_expression="cron(0 1 * * ? *)"),  # noqa
         )
