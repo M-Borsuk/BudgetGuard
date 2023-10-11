f4083a14eef867a5c5bde474da9d0d9bf49d8756
M-Borsuk <mtszborsuk212@gmail.com>
2023-10-11 23:07:11 +0200

    Black reformatting

--- budgetguard/budgetguard/lambda/ingestion.py
+++ budgetguard/budgetguard/lambda/ingestion.py
@@ -1,6 +1,7 @@
 from budgetguard.main import run
 from datetime import datetime
 
+
 def lambda_handler(event, context):
     task = "ingest_account_data"
     partition_id = datetime.now().strftime("%Y%m%d")
